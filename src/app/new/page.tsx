7bc4495975faf4c65400d8fd5f6137f6d7b4b206
Libo033 <113865453+Libo033@users.noreply.github.com>
2024-03-06 00:02:14 -0300

    En proceso: PeticionBody

--- src/app/new/page.tsx
+++ src/app/new/page.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import styles from "./page.module.css";
 import PeticionHead from "@/componentes/new/PeticionHead";
+import PeticionBody from "@/componentes/new/PeticionBody";
 
 const NuevaPeticion = () => {
   return (
     <main>
       <PeticionHead />
+      <PeticionBody />
     </main>
   );
 };
