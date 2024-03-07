4d23830d6a0055935a29b9588c2c2373b49d8d86
Libo033 <113865453+Libo033@users.noreply.github.com>
2024-03-07 14:55:03 -0300

    En proceso: componente inicio

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
+import Inicio from "@/componentes/inicio/Inicio";
 import styles from "./page.module.css";
 
 export default function Home() {
-  return <main className={styles.main}></main>;
+  return (
+    <main className={styles.main}>
+      <h1>Proba nuestra API</h1>
+      <Inicio />
+    </main>
+  );
 }
---
+++ src/componentes/inicio/Inicio.tsx
@@ -0,0 +1,85 @@
+"use client";
+import React, { useState } from "react";
+import styles from "./page.module.css";
+import {
+  Button,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  SelectChangeEvent,
+  TextField,
+} from "@mui/material";
+import { Add, Delete } from "@mui/icons-material";
+
+const InicioInputs: React.FC<{
+  value: number;
+  deleteThis: (v: number) => void;
+}> = ({ value, deleteThis }) => {
+  return (
+    <div className={styles.Inicio_Container}>
+      <TextField fullWidth variant="outlined" label="KEY" />
+      <TextField fullWidth variant="outlined" label="VALUE" />
+      <Button
+        variant="outlined"
+        color="error"
+        onClick={() => deleteThis(value)}
+      >
+        <Delete />
+      </Button>
+    </div>
+  );
+};
+
+const InicioMethod: React.FC<{
+  handleChange: (event: SelectChangeEvent) => void;
+  metodo: "POST" | "PUT" | "DELETE";
+}> = ({ handleChange, metodo }) => {
+  return (
+    <FormControl fullWidth>
+      <InputLabel id="demo-simple-select-label">Metodo</InputLabel>
+      <Select
+        labelId="demo-simple-select-label"
+        id="demo-simple-select"
+        label="Metodo"
+        value={metodo}
+        onChange={handleChange}
+      >
+        <MenuItem value={"POST"}>POST</MenuItem>
+        <MenuItem value={"PUT"}>PUT</MenuItem>
+        <MenuItem value={"DELETE"}>DELETE</MenuItem>
+      </Select>
+    </FormControl>
+  );
+};
+
+const Inicio = () => {
+  const [pares, setPares] = useState<number[]>([1]);
+  const [metodo, setMetodo] = useState<"POST" | "PUT" | "DELETE">("POST");
+
+  const handleChangeSelect = (e: SelectChangeEvent) => {
+    setMetodo(e.target.value as "POST" | "PUT" | "DELETE");
+  };
+
+  const handleAddPar = () => {
+    setPares([...pares, pares[pares.length - 1] + 1]);
+  };
+
+  const handleDelete = (v: number) => {
+    console.log(v);
+  };
+
+  return (
+    <div className={styles.Inicio}>
+      <InicioMethod handleChange={handleChangeSelect} metodo={metodo} />
+      {pares.map((p) => (
+        <InicioInputs key={p} value={p} deleteThis={handleDelete} />
+      ))}
+      <Button fullWidth variant="contained" onClick={handleAddPar}>
+        <Add />
+      </Button>
+    </div>
+  );
+};
+
+export default Inicio;
